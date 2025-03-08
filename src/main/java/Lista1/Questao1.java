437d7e26df01d6fa08209a8bfcad3e669944e5d1
Gabriel Victor Souza Lopes <gabrieelvictor26@gmail.com>
2025-03-08 14:04:59 -0300

    Initial commit

---
+++ src/main/java/Lista1/Questao1.java
@@ -0,0 +1,12 @@
+package Lista1;
+
+/* Faça um programa que leia um vetor A com 15 elementos inteiros. Em seguida, construa
+um vetor B do mesmo tipo, em que cada elemento de B deva ser o resultado do fatorial
+correspondente de cada elemento de A. Imprima na tela os vetores A e B. */
+
+public class Questao1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
